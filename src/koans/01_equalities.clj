5a741ca006e71f87453bb9965849247552fbaa22
Florian Hassanen <florian.hassanen@gmail.com>
2014-10-07 17:34:26 +0200

    round 01

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -3,34 +3,34 @@
 
 (meditations
   "We shall contemplate truth by testing reality, via equality"
-  (= __ true)
+  (= (not false) true)
 
   "To understand reality, we must compare our expectations against reality"
-  (= __ (+ 1 1))
+  (= (- 3 1) (+ 1 1))
 
   "You can test equality of many things"
-  (= (+ 3 4) 7 (+ 2 __))
+  (= (+ 3 4) 7 (+ 2 (* 5 1 1 1 1)))
 
   "Some things may appear different, but be the same"
-  (= __ (= 2 2/1))
+  (= (= 1024/2048 1/2) (= 2 2/1))
 
   "You cannot generally float to heavens of integers"
-  (= __ (= 2 2.0))
+  (= (not true) (= 2 2.0))
 
   "But a looser equality is also possible"
-  (= __ (== 2.0 2))
+  (= (and true true true true true) (== 2.0 2))
 
   "Something is not equal to nothing"
-  (= __ (not (= 1 nil)))
+  (= (or true 0 0 0 0 0 nil) (not (= 1 nil)))
 
   "Strings, and keywords, and symbols: oh my!"
-  (= __ (= "foo" :foo 'foo))
+  (= (= 0 1) (= "foo" :foo 'foo))
 
   "Make a keyword with your keyboard"
-  (= :foo (keyword __))
+  (= :foo (keyword :foo) (keyword "foo") (keyword 'foo))
 
   "Symbolism is all around us"
-  (= 'foo (symbol __))
+  (= 'foo (symbol 'foo) (symbol (name :foo)) (symbol "foo"))
 
   "When things cannot be equal, they must be different"
-  (not= :fill-in-the-blank __))
+  (not= :fill-in-the-blank 'fill-in-the-blank))
